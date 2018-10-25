e4d86487c54e90b0c7222c3f8725a02170b7b913
apple <rocker_pg@163.com>
2018-10-25 16:44:19 +0800

    eureka客户端(服务消费方) 结合feign获取服务，进行调用

---
+++ SpringCloudEurekaClientConsumerFeign/src/main/java/cn/rocker/springcloudeurekaclientconsumerfeign/SpringCloudEurekaClientConsumerFeignApplication.java
@@ -0,0 +1,12 @@
+package cn.rocker.springcloudeurekaclientconsumerfeign;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringCloudEurekaClientConsumerFeignApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringCloudEurekaClientConsumerFeignApplication.class, args);
+    }
+}
