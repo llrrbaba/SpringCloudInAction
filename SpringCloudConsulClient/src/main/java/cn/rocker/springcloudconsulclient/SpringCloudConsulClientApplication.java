871874f2447e99ec722ceb2abc1ca0556dce7fd3
apple <rocker_pg@163.com>
2018-10-25 14:05:11 +0800

    consul客户端(服务提供方)

---
+++ SpringCloudConsulClient/src/main/java/cn/rocker/springcloudconsulclient/SpringCloudConsulClientApplication.java
@@ -0,0 +1,12 @@
+package cn.rocker.springcloudconsulclient;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringCloudConsulClientApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringCloudConsulClientApplication.class, args);
+    }
+}
