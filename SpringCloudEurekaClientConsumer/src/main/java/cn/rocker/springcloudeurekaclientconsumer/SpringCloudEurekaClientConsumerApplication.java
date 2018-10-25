bbaa3364d73c90e60a38748b26b788777ca152fc
apple <rocker_pg@163.com>
2018-10-25 15:53:54 +0800

    eureka客户端(服务消费方)

--- SpringCloudEurekaClientConsumer/src/main/java/cn/rocker/springcloudeurekaclientconsumer/SpringCloudEurekaClientConsumerApplication.java
+++ SpringCloudEurekaClientConsumer/src/main/java/cn/rocker/springcloudeurekaclientconsumer/SpringCloudEurekaClientConsumerApplication.java
@@ -2,9 +2,19 @@ package cn.rocker.springcloudeurekaclientconsumer;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
 
+@EnableDiscoveryClient
 @SpringBootApplication
 public class SpringCloudEurekaClientConsumerApplication {
+
+    @Bean
+    public RestTemplate restTemplate(){
+        return new RestTemplate();
+    }
+
 
     public static void main(String[] args) {
         SpringApplication.run(SpringCloudEurekaClientConsumerApplication.class, args);
