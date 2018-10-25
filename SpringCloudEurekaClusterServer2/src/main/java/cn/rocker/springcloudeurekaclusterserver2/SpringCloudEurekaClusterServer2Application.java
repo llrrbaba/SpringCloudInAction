cbe98a2a31dcda7e84fb9faa8903fbd691eac822
apple <rocker_pg@163.com>
2018-10-25 14:08:24 +0800

    eureka双节点服务注册中心集群-节点2

---
+++ SpringCloudEurekaClusterServer2/src/main/java/cn/rocker/springcloudeurekaclusterserver2/SpringCloudEurekaClusterServer2Application.java
@@ -0,0 +1,12 @@
+package cn.rocker.springcloudeurekaclusterserver2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringCloudEurekaClusterServer2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringCloudEurekaClusterServer2Application.class, args);
+    }
+}
